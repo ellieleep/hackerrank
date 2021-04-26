c2d0d5d557f7fe49e2ece3c60ff0c02fcf7bc18a
ellieleep <32715340+ellieleep@users.noreply.github.com>
2021-04-26 18:24:56 -0400

    completed

--- itertoolspermutations.py
+++ itertoolspermutations.py
@@ -1,4 +1,5 @@
 from itertools import permutations
 
-string, length = map(lambda x: x, input().split(' '))
-[print(*z, sep='') for z in sorted(list(permutations(string, int(length))))]
+string, length = input().split(' ')
+for z in sorted(list(permutations(string, int(length)))):
+    print(*z, sep='')
