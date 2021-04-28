267c4fa0c2bac39890c5577925d23e1768437400
ellieleep <32715340+ellieleep@users.noreply.github.com>
2021-04-28 09:09:01 -0400

    completed

--- defaultdict.py
+++ defaultdict.py
@@ -5,17 +5,12 @@ n, m = [int(x) for x in input().split(' ')]
 listN = [input() for inputN in range(n)]
 listM = [input() for inputM in range(m)]
 
-dictFromLists = defaultdict(list)
+dictFromLists = defaultdict(lambda: -1)
 
-[dictFromLists['listN'].append(wordN) for wordN in listN]
-[dictFromLists['listM'].append(wordM) for wordM in listM]
-
-for wordM in dictFromLists['listM']:
-    if wordM not in dictFromLists['listN']:
-        print(-1)
-    for i, wordN in enumerate(dictFromLists['listN']):
-        if wordM == wordN:
-            print(i+1, end=' ')
-
-    print()
+for iM, wordM in enumerate(listM):
+    for iN, wordN in enumerate(listN):
+        if wordN == wordM and wordM not in dictFromLists.keys():
+            dictFromLists[wordM] = iN + 1
+        elif wordN == wordM and wordM in dictFromLists.keys():
+            dictFromLists[wordM].append(iN + 1)
 
