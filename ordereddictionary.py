01479e102c48b3e7c4b339831577c1738a28fcb2
ellieleep <32715340+ellieleep@users.noreply.github.com>
2021-04-29 21:46:36 -0400

    completed

---
+++ ordereddictionary.py
@@ -0,0 +1,19 @@
+from collections import OrderedDict
+import re
+
+numberItems = input()
+
+itemsBought = OrderedDict()
+
+for items in range(int(numberItems)):
+    itemPrice = input()
+    regexObj = re.search(r'(\D+)(\d+)', itemPrice)
+    item = regexObj.group(1).strip()
+    price = regexObj.group(2)
+    if item not in itemsBought:
+        itemsBought[item] = int(price)
+    else:
+        itemsBought[item] += int(price)
+
+for key, value in itemsBought.items():
+    print(key, value)
