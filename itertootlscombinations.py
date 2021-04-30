9b284c2c83d7ea5a52aab4f60278f8bca31c7b37
ellieleep <ellie.leepierce@gmail.com>
2021-04-30 15:47:21 -0400

    added

---
+++ itertootlscombinations.py
@@ -0,0 +1,10 @@
+import itertools
+
+string, combinations = input().split(' ')
+
+resultList = []
+
+for numComb in range(1, int(combinations)+1):
+    combo = list(itertools.combinations(sorted(string), int(numComb)))
+    for item in combo:
+        print(''.join(item))
