0b94057db67bad9d6a0fc47c3007c51c843ceeaf
ellieleep <32715340+ellieleep@users.noreply.github.com>
2021-05-01 01:28:20 -0400

    completed

---
+++ itertoolscombinationswithreplacement.py
@@ -0,0 +1,7 @@
+import itertools
+
+string, combinations = input().split(' ')
+
+resultList = list(itertools.combinations_with_replacement(sorted(string), int(combinations)))
+for item in resultList:
+        print(''.join(item), sep='')
