77f181c6b7b146e04fafd23acecc47adb822243f
ellieleep <ellie.leepierce@gmail.com>
2021-05-07 11:03:10 -0400

    added

--- repeatedstring.py
+++ repeatedstring.py
@@ -1,13 +1,19 @@
 import itertools
+import string
+import numpy as np
 
 s = input()
 n = int(input())
 
-lettercount = 0
+alphabet = dict(zip(string.ascii_lowercase, [int(x) for x in range(1, 27)]))
 
-for i, letter in enumerate(itertools.cycle(list(s))):
-    if letter == 'a' and i < n:
-        lettercount += 1
+stringArray = np.array([alphabet[x] for x in s])
+
+letterCount = 0
+
+for i, letter in enumerate(itertools.cycle(np.nditer(stringArray))):
+    if letter == 1 and i < n:
+        letterCount += 1
     elif i == n:
-        print(lettercount)
+        print(letterCount)
         break
