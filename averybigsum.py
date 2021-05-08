d052b0a41008152e2e0a22f9b6a0c542a18780b8
ellieleep <32715340+ellieleep@users.noreply.github.com>
2021-05-07 20:24:11 -0400

    completed

---
+++ averybigsum.py
@@ -0,0 +1,5 @@
+numDigits = int(input())
+
+digitArray = [int(x) for x in input().split()]
+
+print(sum(digitArray))
---
+++ comparingthetriplets.py
@@ -0,0 +1,8 @@
+scores = {}
+
+aScores = [int(x) for x in input().split()]
+bScores = [int(y) for y in input().split()]
+
+scores['a'] = aScores*
+
+pass
